242c77c76c557221c0d4af3719633643682296e1
Vennelab08 <vennelab08@gmail.com>
2024-09-28 19:03:30 +0530

    modified errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import image from "next/image";
+
 
 export default function Page() {
   return <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
-    <div> 
-      <h1 className = "text-6xl">welcome to Nextjs App </h1>
-     
-
+  <div> 
+    <h1 className = "text-6xl">welcome to Nextjs App </h1>
       <ol className="list-inside list-decimal text-lg text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started 
@@ -17,8 +15,8 @@ export default function Page() {
 
 
       
-    </div>
-    </div>
+  </div>
+      </div>
         
 }
 
